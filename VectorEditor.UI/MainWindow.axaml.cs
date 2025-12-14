5aa33c2ec67f70e010a28602386b798e1ac67a6e
ThePlayMK <theplaymk@gmail.com>
2025-12-14 03:29:50 +0100

    Somewhat good starting UI

--- VectorEditor.UI/MainWindow.axaml.cs
+++ VectorEditor.UI/MainWindow.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Interactivity;
 using Avalonia.Controls.Shapes;
 using Avalonia.Input;
 using Avalonia.Media;
+using Material.Icons;
 using System;
 using Avalonia.Controls.Primitives;
 
@@ -11,139 +12,52 @@ namespace VectorEditor.UI
 {
     public partial class MainWindow : Window
     {
-
-        private Point? _startPoint;
-        private Polyline? _currentPolyline;
-        private bool _isDrawing;
-        private IBrush _selectedColor = Brushes.Black;
-        private int _size = 5;
-
+        private Button? _lastSelectedButton;
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        int _selectedTool = -1;
-        void OnSelectTool(object? sender, RoutedEventArgs e)
+        public void ToggleMenu(object sender, RoutedEventArgs args)
         {
-            var button = sender as Button;
-            if (button?.Tag == null) return;
-            _selectedTool = Convert.ToInt32(button.Tag);
-            _startPoint = null;
-            _isDrawing = false;
-            _currentPolyline = null;
+            MenuOverlay.IsVisible = !MenuOverlay.IsVisible;
         }
-        void OnSelectColor(object? sender, RoutedEventArgs e)
+        void ToggleLayer(object sender, RoutedEventArgs args)
         {
-            var button = sender as Button;
-            if (button?.Tag == null) return;
-            switch (Convert.ToInt32(button.Tag))
+            var clickedButton = sender as Button;
+            if (clickedButton == null) return;
+            var iconButton = clickedButton.Content as Material.Icons.Avalonia.MaterialIcon;
+            if(iconButton==null) return;
+            if (iconButton.Kind == MaterialIconKind.Eye)
             {
-                case 0:
-                    _selectedColor = Brushes.Red;
-                    break;
-                case 1:
-                    _selectedColor = Brushes.Blue;
-                    break;
-                case 2:
-                    _selectedColor = Brushes.Yellow;
-                    break;
-                case 3:
-                    _selectedColor = Brushes.Green;
-                    break;
-                case 4:
-                    _selectedColor = Brushes.Black;
-                    break;
-                case 5:
-                    _selectedColor = Brushes.White;
-                    break;
-                case 6:
-                    _selectedColor = Brushes.Orange;
-                    break;
-                case 7:
-                    _selectedColor = Brushes.Brown;
-                    break;
-                default:
-                    _selectedColor = Brushes.Black;
-                    break;
+                iconButton.Kind = MaterialIconKind.EyeOff;
             }
+            else iconButton.Kind = MaterialIconKind.Eye;
         }
-        void SizeSlider(object? sender, RangeBaseValueChangedEventArgs e)
+        void SelectColor(object sender, RoutedEventArgs args)
         {
-            var slider = sender as Slider;
-            if (slider == null) return;
-            _size = (int)slider.Value;
-        }
-        void OnCanvasPointerPressed(object? sender, PointerPressedEventArgs e)
-        {
-            var pointerPoint = e.GetCurrentPoint(sender as Control);
-            var currentPosition = pointerPoint.Position;
-            switch (_selectedTool)
+            var clickedButton = sender as Button;
+            if (clickedButton == null) return;
+            if (_lastSelectedButton != null)
             {
-                case 0:
-                    CreateDot(currentPosition);
-                    _startPoint = null;
-                    break;
-                case 1:
-                    if (_startPoint == null)
-                    {
-                        _startPoint = currentPosition;
-                    }
-                    else
-                    {
-                        Line line = new Line
-                        {
-                            StartPoint = _startPoint.Value,
-                            EndPoint = currentPosition,
-                            Stroke = _selectedColor,
-                            StrokeThickness = _size
-                        };
-                        MyDrawingCanvas.Children.Add(line);
-                        _startPoint = null;
-                    }
-                    break;
-                case 2:
-                    _isDrawing = true;
-                    CreateDot(currentPosition);
-                    _currentPolyline = new Polyline
-                    {
-                        Stroke = _selectedColor,
-                        StrokeThickness = _size
-                    };
-                    _currentPolyline.Points.Add(currentPosition);
-                    MyDrawingCanvas.Children.Add(_currentPolyline);
-                    break;
+                _lastSelectedButton.BorderThickness = new Thickness(0);
             }
+            clickedButton.BorderBrush = Brushes.White;
+            clickedButton.BorderThickness = new Thickness(3);
+            clickedButton.CornerRadius = new CornerRadius(4);
+            _lastSelectedButton = clickedButton;
         }
-        void OnCanvasPointerMoved(object? sender, PointerEventArgs e)
+        void SelectTool(object sender, RoutedEventArgs args)
         {
-            if (_selectedTool == 2 && _isDrawing && _currentPolyline != null)
+            var clickedButton = sender as Button;
+            if (clickedButton == null) return;
+            if (_lastSelectedButton != null)
             {
-                var pointerPoint = e.GetCurrentPoint(sender as Control);
-                _currentPolyline.Points.Add(pointerPoint.Position);
+                _lastSelectedButton.BorderThickness = new Thickness(0);
             }
-        }
-        void OnCanvasPointerReleased(object? sender, PointerReleasedEventArgs e)
-        {
-            if (_selectedTool == 2)
-            {
-                var pointerPoint = e.GetCurrentPoint(sender as Control);
-                CreateDot(pointerPoint.Position);
-                _isDrawing = false;
-                _currentPolyline = null;
-            }
-        }
-        private void CreateDot(Point position)
-        {
-            var ellipse = new Ellipse
-            {
-                Width = _size,
-                Height = _size,
-                Fill = _selectedColor
-            };
-            Canvas.SetLeft(ellipse, position.X - (_size / 2.0));
-            Canvas.SetTop(ellipse, position.Y - (_size / 2.0));
-            MyDrawingCanvas.Children.Add(ellipse);
+            clickedButton.BorderBrush = Brushes.White;
+            clickedButton.BorderThickness = new Thickness(3);
+            clickedButton.CornerRadius = new CornerRadius(4);
+            _lastSelectedButton = clickedButton;
         }
     }
 }
