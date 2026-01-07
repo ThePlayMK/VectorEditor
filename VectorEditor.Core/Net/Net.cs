6b0d5304c40cc29a78c788349f5493e697637943
Dzermi <remusiekpl05@gmail.com>
2026-01-07 16:40:57 +0100

    net wstepnie

--- VectorEditor.Core/Net/Net.cs
+++ VectorEditor.Core/Net/Net.cs
@@ -1,6 +1,40 @@
-namespace VectorEditor.Core.Net;
+using System;
+using VectorEditor.Core.Structures; 
 
-public class Net
+namespace VectorEditor.Core.Net
 {
-    
+    public class EditorGrid
+    {
+        // Rozmiar pojedynczej kratki (np. 20 pikseli)
+        public double CellSize { get; set; } = 20.0;
+
+        // Czy siatka jest widoczna i aktywna
+        public bool IsEnabled { get; set; } = false;
+
+        // Czy przyciąganie jest włączone (można widzieć siatkę, ale nie przyciągać)
+        public bool SnapToGridEnabled { get; set; } = false;
+
+        public EditorGrid(double cellSize = 20.0)
+        {
+            CellSize = cellSize;
+        }
+
+        /// <summary>
+        /// Główna logika: bierze punkt i zwraca nowy punkt "przyciągnięty" do siatki
+        /// </summary>
+        public Point Snap(Point inputPoint)
+        {
+            if (!SnapToGridEnabled || !IsEnabled)
+            {
+                // Jeśli siatka wyłączona, zwracamy oryginał
+                return inputPoint;
+            }
+
+            // Matematyka zaokrąglania do najbliższej wielokrotności CellSize
+            double snappedX = Math.Round(inputPoint.X / CellSize) * CellSize;
+            double snappedY = Math.Round(inputPoint.Y / CellSize) * CellSize;
+
+            return new Point(snappedX,snappedY);
+        }
+    }
 }
