19afb3d78754178dec9c2b3c196d9a8020c3c774
Kkkuc <91897@student.pb.edu.pl>
2026-01-11 15:13:30 +0100

    for fuck sake

--- VectorEditor.Core/Command/AddShapeCommand.cs
+++ VectorEditor.Core/Command/AddShapeCommand.cs
@@ -9,15 +9,8 @@ public class AddShapeCommand(IShapeBuilder builder, Layer targetLayer) : IComman
 
     public void Execute()
     {
-        //testy
-        //System.Diagnostics.Debug.WriteLine($"[CMD] Execute start. Warstwa ID: {targetLayer.GetHashCode()}");
-        //if (targetLayer.IsBlocked){
-        //System.Diagnostics.Debug.WriteLine("[CMD] BLOKADA! Warstwa jest zablokowana (IsBlocked = true).");
-          //  return;
-        //}
         _shape ??= builder.Build(); // ✅ tylko raz
         targetLayer.Add(_shape);
-        //System.Diagnostics.Debug.WriteLine($"[CMD] SUKCES. Dodałem kształt. Nowa liczba dzieci: {targetLayer.GetChildren().ToList().Count}");
     }
 
     public void Undo()
